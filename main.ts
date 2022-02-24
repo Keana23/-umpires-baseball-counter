5f7678744d4cf917eb58cc7160e1b422a2a034d1
Keana23 <100366128+Keana23@users.noreply.github.com>
2022-02-24 11:09:21 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let number_of_balls = 0
+let number_of_strikes = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(4)
+    number_of_balls = 4
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(3)
+    number_of_strikes = 3
+})
+basic.forever(function () {
+	
+})
